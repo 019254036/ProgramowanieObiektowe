ea04efc3ca7c24787b63ee5fc2441996ab97e30b
RudyPrzyjaciel <szynks20@gmail.com>
2020-06-04 18:55:43 +0200

    deleted test folder

--- tests/main_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-
-int main(){
-	//test_header();
-	//tablica_Dynamiczna_Jedno();
-	test_przestrzeni();
-	return 0;
-}
--- tests/test.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void test_header();
-void tablica_Dynamiczna_Jedno();
-int test_przestrzeni();
